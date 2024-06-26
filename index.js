0db3feb9e6844598fe957bc6b176314e3c54c1c4
NidhiPednekar <146639573+NidhiPednekar@users.noreply.github.com>
2024-06-26 17:30:44 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var mysql = require("mysql")
 
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: 'Colleges'
-}
-);
+var express = require('express')
+var app = express()
+
+const PORT = process.env.PORT || 3003;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/register.html')
+});
+
+app.listen(PORT, () => {
+    //console.log(`Server is running at http://localhost:${PORT}`);
+});
