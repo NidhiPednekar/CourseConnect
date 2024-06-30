95e7638486f5a9247e9804564db8448496fb3f08
NidhiPednekar <146639573+NidhiPednekar@users.noreply.github.com>
2024-06-30 19:57:43 +0530

    cleared index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 
-var express = require('express')
-var app = express()
-
-const PORT = process.env.PORT || 3003;
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/register.html')
-});
-
-app.listen(PORT, () => {
-    //console.log(`Server is running at http://localhost:${PORT}`);
-});
